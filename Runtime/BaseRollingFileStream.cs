51746ea1a855988934dcbd1ce8beab7e0041e03d
zilch <wlgys8>
2020-11-22 13:41:24 +0800

    first commit

---
+++ Runtime/BaseRollingFileStream.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MS.FileRoller{
+    public abstract class BaseRollingFileStream
+    {
+
+        private string _dir;
+        private string _fileName;    
+        private string _filePath;
+        private FileStream _fileStream;
+
+        public BaseRollingFileStream(string filePath){
+            _filePath = filePath;
+            _dir = Path.GetDirectoryName(filePath);
+            _fileName = Path.GetFileName(filePath);
+        }
+
+        public string path{
+            get{
+                return _filePath;
+            }
+        }
+
+        public string dir{
+            get{
+                return _dir;
+            }
+        }
+
+        public string fileName{
+            get{
+                return _fileName;
+            }
+        }
+
+        private FileStream EnsureFileStream(){
+            if(_fileStream == null){
+                if(!Directory.Exists(_dir)){
+                    Directory.CreateDirectory(_dir);
+                }
+                _fileStream = new FileStream(this._filePath,FileMode.Append);
+                OnFileStreamCreated();
+            }
+            return _fileStream;
+        }
+
+        public void Close(){
+            if(_fileStream != null){
+                _fileStream.Close();
+                _fileStream = null;
+                OnFileStreamClosed();
+            }
+        }
+
+        public long Length{
+            get{
+                return _fileStream == null ? 0 : _fileStream.Length;
+            }
+        }
+
+
+        public async Task WriteAsync(byte[] buffer,int offset,int count){
+            var fs = EnsureFileStream();
+            await fs.WriteAsync(buffer,offset,count);
+            await RollAsyncIfNeed();
+        }
+
+        public Task FlushAsync(){
+            if(_fileStream != null){
+                return _fileStream.FlushAsync();
+            }
+            return Task.CompletedTask;
+        }
+
+        public void Roll(){
+            Close();
+            StartRoll();            
+        }
+        
+
+        private async Task RollAsyncIfNeed(){
+            if(!ShouldRoll()){
+                return;
+            }
+            await FlushAsync();
+            Close();
+            StartRoll();
+        }
+
+
+        protected abstract bool ShouldRoll();
+
+        protected abstract void StartRoll();
+
+        protected virtual void OnFileStreamCreated(){}
+        protected virtual void OnFileStreamClosed(){}
+        
+
+    }
+}
---
+++ Runtime/DateRollingFileStream.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace MS.FileRoller{
+
+    public class DateRollingFileStream:BaseRollingFileStream
+    {
+
+        public class Options{
+            public int maxBackups = 3;
+            public int maxFileSize = 1024 * 10;
+            public bool keepExts = false;
+        }
+
+        private int _maxBackups;
+        private int _maxFileSize;
+
+        private bool _keepExts;
+
+        private System.DateTime _writingDate;
+
+        public DateRollingFileStream(string filePath,Options options):base(filePath){
+            _maxBackups = Mathf.Max(1,options.maxBackups);
+            _maxFileSize = Mathf.Max(1024 * 10,options.maxFileSize);
+            _keepExts = options.keepExts;
+        }
+
+
+        protected override void OnFileStreamCreated()
+        {
+            base.OnFileStreamCreated();
+            _writingDate = System.DateTime.Now.Date;
+        }
+
+        protected override bool ShouldRoll()
+        {
+             var date = System.DateTime.Now.Date;
+             if(_writingDate != date){
+                 //date changed
+                 return true;
+             }
+            if(this.Length >= _maxFileSize){
+                //file size exceed
+                return true;
+            }
+            return false;
+        }
+
+        protected override void StartRoll()
+        {
+            var date = System.DateTime.Now.Date;
+            var dateStr = date.ToString("yyyyMMdd");
+            var datedFiles = FileRollUtil.GetDatedBackupFiles(dir,fileName,_keepExts);
+            while(datedFiles.Count >= _maxBackups){
+                var oldest = datedFiles[datedFiles.Count - 1];
+                datedFiles.RemoveAt(datedFiles.Count - 1);
+                File.Delete(oldest.path);
+            }
+            var todayFileCount = CountOfFilesToday(datedFiles);
+
+            var todayFileName = FileRollUtil.GetDatedFileName(fileName,date,0,_keepExts);
+            FileRollUtil.RollFiles(dir,todayFileName,todayFileCount + 1,_keepExts);
+            if(File.Exists(path)){
+                File.Move(path,Path.Combine(dir,todayFileName));
+            }
+        }
+
+        private int CountOfFilesToday(List<DatedFileInfo> fileInfos){
+            var date = System.DateTime.Now.Date;
+            int count = 0;
+            foreach(var f in fileInfos){
+                if(f.date == date){
+                    count ++;
+                }
+            }
+            return count;
+        }
+
+
+    }
+
+}
